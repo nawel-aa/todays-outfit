8b3d6d6552e1deb93ca2e691fdc849c719b2e642
Nawel Aammar <nawel.aa@hotmail.fr>
2021-01-14 16:34:03 -0500

    create and index actions for worn

--- app/controllers/api/v1/worn_controller.rb
+++ app/controllers/api/v1/worn_controller.rb
@@ -1,1 +1,40 @@
 # MEMO: created_at.localtime to display in local time zone
+
+class Api::V1::WornController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User, only: :create
+  before_action :set_item, only: :create
+
+  def index
+    @worn = policy_scope(Worn).includes(:outfit)
+  end
+
+  def create
+    # Set the properties and link the user
+    @worn = Worn.new(worn_params)
+    @worn.user = current_user
+    @worn.outfit = Outfit.find(params[:outfit])
+
+    authorize @worn
+
+    if @worn.save
+      render json: { success: "The choice ##{@worn.id} has been created." }, status: :created
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def set_worn
+    @worn = Worn.find(params[:id])
+    authorize @worn
+  end
+
+  def worn_params
+    params.require(:worn).permit(:rejected)
+  end
+
+  def render_error
+    render json: { errors: @worn.errors.full_messages }, status: :unprocessable_entity
+  end
+end
--- app/policies/worn_policy.rb
+++ app/policies/worn_policy.rb
@@ -1,7 +1,11 @@
 class WornPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(user: user)
     end
+  end
+
+  def create?
+    true
   end
 end
